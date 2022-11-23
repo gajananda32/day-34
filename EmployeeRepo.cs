9c4a4ab3dd141690f12dce64d5d5ed08083d0cf9
gajananda32 <113405579+gajananda32@users.noreply.github.com>
2022-11-23 19:13:20 +0530

    Added Ado.net

---
+++ EmployeeRepo.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpPayroll
+{
+    public class EmployeeRepo
+    {
+        public static string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=payroll_service;Integrated Security=True";
+        SqlConnection connection = new SqlConnection(connectionString);
+
+        public void GetAllEmployee()
+        {
+            try
+            {
+                EmployeeModel model = new EmployeeModel();
+
+                using (this.connection)
+                {
+                    string query = @"select EmployeeID,EmployeeName,PhoneNumber,Address,Department,Gender,BasicPay,Deductions,
+                                    TaxablePay,Tax,NetPay,StartDate,City,Country from employee_payroll";
+
+                    SqlCommand cmd = new SqlCommand(query, connection);
+                    this.connection.Open();
+
+                    SqlDataReader dr = cmd.ExecuteReader();
+
+                    if (dr.HasRows)
+                    {
+                        while (dr.Read())
+                        {
+                            model.EmployeeID = dr.GetInt32(0);
+                            model.EmployeeName = dr.GetString(1);
+                            model.PhoneNumber = dr.GetString(2);
+                            model.Address = dr.GetString(3);
+                            model.Department = dr.GetString(4);
+                            model.Gender = Convert.ToChar(dr.GetString(5));
+                            model.BasicPay = dr.GetDouble(6);
+                            model.Deductions = dr.GetDouble(7);
+                            model.TaxablePay = dr.GetDouble(8);
+                            model.Tax = dr.GetDouble(9);
+                            model.NetPay = dr.GetDouble(10);
+                            model.StartDate = dr.GetDateTime(11);
+                            model.City = dr.GetString(12);
+                            model.Country = dr.GetString(13);
+                            Console.WriteLine(model.EmployeeID + " - " + model.EmployeeName + " - " + model.PhoneNumber);
+
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("No data found");
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+            finally
+            {
+                this.connection.Close();
+            }
+        }
+
+        public bool AddEmployee(EmployeeModel employee)
+        {
+            try
+            {
+                using (this.connection)
+                {
+                    SqlCommand command = new SqlCommand("SqAddEmployeeDetails", this.connection);
+                    command.CommandType = System.Data.CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@EmployeeName", employee.EmployeeName);
+                    command.Parameters.AddWithValue("@PhoneNumber", employee.PhoneNumber);
+                    command.Parameters.AddWithValue("@Address", employee.Address);
+                    command.Parameters.AddWithValue("@Department", employee.Department);
+                    command.Parameters.AddWithValue("@Gender", employee.Gender);
+                    command.Parameters.AddWithValue("@BasicPay", employee.BasicPay);
+                    command.Parameters.AddWithValue("@Deductions", employee.Deductions);
+                    command.Parameters.AddWithValue("@TaxablePay", employee.TaxablePay);
+                    command.Parameters.AddWithValue("@Tax", employee.Tax);
+                    command.Parameters.AddWithValue("@NetPay", employee.NetPay);
+                    command.Parameters.AddWithValue("@StartDate", DateTime.Now);
+                    command.Parameters.AddWithValue("@City", employee.City);
+                    command.Parameters.AddWithValue("@Country", employee.Country);
+
+                    this.connection.Open();
+
+                    var result = command.ExecuteNonQuery();
+
+                    this.connection.Close();
+                    if (result != 0)
+                    {
+                        return true;
+                    }
+                    return false;
+
+
+                }
+
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+            finally
+            {
+                this.connection.Close();
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿namespace EmpPayroll
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee PayRoll");
+
+            EmployeeRepo employeeRepo = new EmployeeRepo();
+            EmployeeModel employeeModel = new EmployeeModel();
+
+            employeeModel.EmployeeName = "Amogh";
+            employeeModel.PhoneNumber = "1234567899";
+            employeeModel.Address = "12 streeet";
+            employeeModel.Department = "Developer";
+            employeeModel.Gender = 'M';
+            employeeModel.BasicPay = 22000.00;
+            employeeModel.Deductions = 1500.00;
+            employeeModel.Tax = 300.00;
+            employeeModel.TaxablePay = 200.00;
+            employeeModel.NetPay = 30000.00;
+            employeeModel.City = "Pune";
+            employeeModel.Country = "India";
+
+             employeeRepo.AddEmployee(employeeModel);
+
+            //employeeRepo.GetAllEmployee();
+        }
+    }
+}
+
